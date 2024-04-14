ef1ff3eefc9380fa341fb20c41a21d0b697ea858
dionisiyKDO <135739796+dionisiyKDO@users.noreply.github.com>
2024-04-14 22:19:18 +0300

    Load checkpoints and classify image
    Added functions to load saved checkpoints to model and function to classify one image

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
-import neural_network
+from neural_network import *
 from data_analyze import *
 
 if __name__ == '__main__':
     # VGG16     VGG19   AlexNet     ResNet50
     # InceptionResNetV2 InceptionV3   
-    # EfficientNetV2    EfficientNetB5
-    # AlexNet_own       test    OwnV1
+    # EfficientNetV2    AlexNet_own
+    # test      OwnV1   OwnV2
 
-    network_name = 'test'
-    epochs = 50
-    batch_size = 32
+    # network_name = 'OwnV2'
+    # epochs = 35
+    # batch_size = 32
 
-    earlystop = False
-    model_summary = True
-    save = False
+    # earlystop = False
+    # model_summary = True
+    # save = False
 
-    logging = True
-    graphs = True
+    # logging = True
+    # graphs = True
     
-    nn_model, nn_result = neural_network.NN(network_name=network_name, 
-                                            epochs=epochs, 
-                                            batch_size=batch_size,
-                                            earlystop=earlystop,
-                                            model_summary=model_summary, 
-                                            logging=logging,
-                                            save=save,
-                                            graphs=graphs
-                                            )
+    # nn_model, nn_result = NN(network_name=network_name, 
+    #                                         epochs=epochs, 
+    #                                         batch_size=batch_size,
+    #                                         earlystop=earlystop,
+    #                                         model_summary=model_summary, 
+    #                                         logging=logging,
+    #                                         save=save,
+    #                                         graphs=graphs
+    #                                         )
     
-    print(f'Test loss: {nn_result[0]:.4f}')
-    print(f'Test accuracy: {nn_result[1]:.4f}')
-    print(f'Training time: {nn_result[2]:.4f}')
+    # print(f'Test loss: {nn_result[0]:.4f}')
+    # print(f'Test accuracy: {nn_result[1]:.4f}')
+    # print(f'Training time: {nn_result[2]:.4f}')
 
+
+
+    # Load the model from a checkpoint
+    checkpoint_path = './checkpoints/OwnV2.epoch36-val_acc0.9922.hdf5'
+    loaded_model = load_model_from_checkpoint(checkpoint_path, 'OwnV2')
+
+    # Classify an image
+    # Te-no.jpg
+    # Te-gl.jpg
+    # Te-pi.jpg
+    # Te-me.jpg
+    image_path = './data/Te-me.jpg'  
+    # image_path = './data/Testing/notumor/Te-no_0141.jpg'
+    predicted_class = classify_image(image_path, loaded_model)
+    print('Predicted class:', predicted_class)
--- neural_network.py
+++ neural_network.py
@@ -1,12 +1,13 @@
 from data_analyze import *
 import os, random, cv2, time, keras, datetime
+from PIL import Image
 
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 import plotly.express as px
+import plotly.graph_objects as go
 import matplotlib.pyplot as plt
-
 from sklearn.metrics import confusion_matrix, classification_report
 
 from keras.preprocessing.image import ImageDataGenerator
@@ -31,23 +32,36 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 def get_model(network_name, shape, num_classes):
+    """
+    Create and return a convolutional neural network model based on the specified architecture.
+
+    Parameters:
+    - network_name (str): Name of the network architecture to create.
+    - shape (tuple): Shape of the input images (height, width, channels).
+    - num_classes (int): Number of output classes.
+
+    Returns:
+    - model: Keras model instance representing the specified architecture.
+    """
     if network_name == 'test':
+        # OwnV2
         model = Sequential([
-            Conv2D(filters=256, kernel_size=(12,12), strides=(5,5), activation='relu', input_shape=shape),
+            Conv2D(filters=64, kernel_size=(12,12), strides=(5,5), activation='relu', input_shape=shape),
             MaxPooling2D(pool_size=(3,3), strides=(2,2)),
             BatchNormalization(),
 
-            Conv2D(filters=512, kernel_size=(6,6), strides=(2,2), activation='relu', padding="same"),
+            Conv2D(filters=128, kernel_size=(6,6), strides=(2,2), activation='relu', padding="same"),
+            MaxPooling2D(pool_size=(3,3), strides=(2,2)),
+            BatchNormalization(),
+
             Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
             MaxPooling2D(pool_size=(3,3), strides=(2,2)),
             BatchNormalization(),
 
             Flatten(),
-            Dense(4096, activation='relu'),
+            Dense(1028, activation='relu'),
             Dropout(0.5),
-            Dense(4096, activation='relu'),
-            Dropout(0.5),
-            Dense(num_classes, activation='softmax')
+            Dense(num_classes,activation='softmax')
         ])
     elif network_name == 'VGG16':
         model = Sequential([
@@ -201,15 +215,103 @@ def get_model(network_name, shape, num_classes):
             Dropout(0.5),
             Dense(num_classes,activation='softmax')
         ])
+    elif network_name == 'OwnV2':
+        # tried decreasing to 16-32-64-512 - too bad
+        # increased to 64-128-256-1028 - performce good (99.22)
+        model = Sequential([
+            Conv2D(filters=64, kernel_size=(12,12), strides=(5,5), activation='relu', input_shape=shape),
+            MaxPooling2D(pool_size=(3,3), strides=(2,2)),
+            BatchNormalization(),
+
+            Conv2D(filters=128, kernel_size=(6,6), strides=(2,2), activation='relu', padding="same"),
+            MaxPooling2D(pool_size=(3,3), strides=(2,2)),
+            BatchNormalization(),
+
+            Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+            MaxPooling2D(pool_size=(3,3), strides=(2,2)),
+            BatchNormalization(),
+
+            Flatten(),
+            Dense(1028, activation='relu'),
+            Dropout(0.5),
+            Dense(num_classes,activation='softmax')
+        ])
+
     else:
         model = None
     return model
 
 
+def load_model_from_checkpoint(checkpoint_path: str, network_name: str):
+    """
+    Load a model from a checkpoint file.
+
+    Parameters:
+    - checkpoint_path (str): Path to the checkpoint file.
+    - network_name (str): Name of the network architecture.
+
+    Returns:
+    - model: Loaded Keras model.
+    """
+
+    model = get_model(network_name, (150, 150, 3), 4)
+    model.load_weights(checkpoint_path)
+
+    return model
+
+def classify_image(image_path: str, model: Sequential):
+    """
+    Classify an image using a trained model.
+
+    Parameters:
+    - image_path (str): Path to the image file.
+    - model: Loaded Keras model.
+
+    Returns:
+    - str: Predicted class label.
+    """
+    class_labels = {
+        'glioma': 0, 
+        'meningioma': 1, 
+        'notumor': 2, 
+        'pituitary': 3, 
+    }
+
+    # Load and preprocess the image
+    img = tf.keras.preprocessing.image.load_img(image_path, target_size=(150, 150))
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
+    img_array /= 255.0
+
+    # Predict class probabilities
+    predictions = model.predict(img_array)
+    predicted_class_index = np.argmax(predictions[0])
+
+    # Map index to class label
+    predicted_class_label = [label for label, index in class_labels.items() if index == predicted_class_index][0]
+
+    return predicted_class_label
 
 
 def NN(network_name='CNN', epochs=20, batch_size=16, earlystop=True, 
        model_summary=False, logging=False, save=False, graphs=False):
+    """
+    Train a convolutional neural network using the specified parameters.
+
+    Parameters:
+    - network_name (str): Name of the network architecture to use.
+    - epochs (int): Number of training epochs.
+    - batch_size (int): Batch size for training.
+    - earlystop (bool): Whether to use early stopping during training.
+    - model_summary (bool): Whether to print the model summary.
+    - logging (bool): Whether to log training progress.
+    - save (bool): Whether to save the trained model.
+    - graphs (bool): Whether to plot training and evaluation graphs.
+
+    Returns:
+    - model: Trained Keras model.
+    - list: List containing [loss, accuracy, training_time].
+    """
     start_time = time.time()
 
     # Initialize parameters
@@ -221,18 +323,18 @@ def NN(network_name='CNN', epochs=20, batch_size=16, earlystop=True,
     img_size = (150, 150)
     img_shape = (img_size[0], img_size[1], channels)
     class_labels = {
-        'notumor': 0,
-        'pituitary': 1,
-        'glioma': 2,
-        'meningioma': 3,
+        'glioma': 0, 
+        'meningioma': 1, 
+        'notumor': 2, 
+        'pituitary': 3, 
     }
     num_classes = len(class_labels.keys())
     # endregion
 
     # Getting data
     # region
-    # train_paths, train_labels = get_data(train_dir)
-    # test_paths, test_labels = get_data(test_dir)
+    train_paths, train_labels = get_data(train_dir)
+    test_paths, test_labels = get_data(test_dir)
     
     train_datagen = ImageDataGenerator(rescale=1./255,
                                        rotation_range=10,
@@ -263,13 +365,57 @@ def NN(network_name='CNN', epochs=20, batch_size=16, earlystop=True,
     validation_steps = test_generator.samples // batch_size
     # endregion
     
+    # Plotting data distribution
+    # region
+    CLASS_TYPES = ['pituitary', 'notumor', 'meningioma', 'glioma']
+
+    # Calculate class counts for training and testing data
+    train_class_counts = [len([x for x in train_labels if x == label]) for label in CLASS_TYPES]
+    test_class_counts = [len([x for x in test_labels if x == label]) for label in CLASS_TYPES]
+
+    # Plotting training data types
+    fig = go.Figure()
+
+    fig.add_trace(go.Pie(labels=[label.title() for label in CLASS_TYPES], 
+                        values=train_class_counts, 
+                        marker=dict(colors=['#FAC500', '#0BFA00', '#0066FA', '#FA0000']), 
+                        textinfo='percent+label+value', 
+                        textfont=dict(size=20),
+                        hole=0.3,
+                        pull=[0.1, 0.1, 0.1, 0.1],
+                        domain={'x': [0, 0.3], 'y': [0.5, 1]}))
+
+    # Plotting distribution of train test split
+    fig.add_trace(go.Pie(labels=['Train', 'Test'], 
+                        values=[len(train_labels), len(test_labels)], 
+                        marker=dict(colors=['darkcyan', 'orange']), 
+                        textinfo='percent+label+value', 
+                        textfont=dict(size=20),
+                        hole=0.3,
+                        pull=[0.1, 0],
+                        domain={'x': [0.35, 0.65], 'y': [0.5, 1]}))
+
+    # Plotting testing data types
+    fig.add_trace(go.Pie(labels=[label.title() for label in CLASS_TYPES], 
+                        values=test_class_counts, 
+                        marker=dict(colors=['#FAC500', '#0BFA00', '#0066FA', '#FA0000']), 
+                        textinfo='percent+label+value', 
+                        textfont=dict(size=20),
+                        hole=0.3,
+                        pull=[0.1, 0.1, 0.1, 0.1],
+                        domain={'x': [0.7, 1], 'y': [0.5, 1]}))
+
+    fig.update_layout(title='Data Distribution', grid={'rows': 1, 'columns': 3})
+    # fig.show()
+    # endregion
+
     # Initialize model
     # region
     model = get_model(network_name, img_shape, num_classes)
 
     if model == None:
         print('=== Incorrect network name ===')
-        return
+        exit()
     if model_summary:
         model.summary()
     
@@ -283,6 +429,8 @@ def NN(network_name='CNN', epochs=20, batch_size=16, earlystop=True,
 
     # Network training
     # region
+    model_es = EarlyStopping(monitor='val_acc', min_delta=1e-9, patience=8, verbose=True)
+    model_rlr = ReduceLROnPlateau(monitor='val_loss', factor=0.3, patience=5, verbose=True)
     model_cp = ModelCheckpoint(
         filepath='./checkpoints/model.epoch{epoch:02d}-val_acc{val_acc:.4f}.hdf5',
         monitor='val_acc',
@@ -290,8 +438,6 @@ def NN(network_name='CNN', epochs=20, batch_size=16, earlystop=True,
         verbose=1,
         save_best_only=True,
         save_weights_only=True)
-    model_es = EarlyStopping(monitor='loss', min_delta=1e-9, patience=8, verbose=True)
-    model_rlr = ReduceLROnPlateau(monitor='val_loss', factor=0.3, patience=5, verbose=True)
     
     history = model.fit(train_generator,
                         steps_per_epoch=steps_per_epoch,
@@ -305,6 +451,12 @@ def NN(network_name='CNN', epochs=20, batch_size=16, earlystop=True,
     # Network evaluation + results of trining
     # region
     loss, accuracy = model.evaluate(test_generator, steps=test_generator.samples//batch_size)
+
+    y_pred = model.predict(test_generator)
+    y_pred = np.argmax(y_pred, axis=1)
+    y_test = test_generator.classes
+    cm = confusion_matrix(y_test, y_pred)
+    # print(cm)
 
     if graphs:
         h1 = history.history
