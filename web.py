434ca2988a652c02d15a54aeb3673933435f99fa
dionisiyKDO <135739796+dionisiyKDO@users.noreply.github.com>
2024-05-13 13:08:01 +0300

    PicoCSS implementation, template and image handling updates

--- web.py
+++ web.py
@@ -1,39 +1,62 @@
 from neural_network import *
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_from_directory, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField
+import os
 
-app = Flask(__name__)
-
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+# Load the model
 checkpoint_path = './checkpoints/OwnV2.epoch36-val_acc0.9922.hdf5'
-
 model = MRIImageClassifier()
 model.load_model_from_checkpoint(checkpoint_path)
 
-@app.route('/', methods=['GET', 'POST'])
+app = Flask(__name__)
+
+# Configuration
+# region
+UPLOAD_FOLDER = 'uploads'
+SECRET_KEY = 'thisisasecretkey'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+
+app.config['UPLOADED_IMAGES_DEST'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = SECRET_KEY
+# app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+# endregion
+
+images = UploadSet('images', IMAGES)
+configure_uploads(app, images)
+
+class UploadForm(FlaskForm):
+    image = FileField(
+        validators=[
+                FileAllowed(images, 'Images only!'),
+                FileRequired('File field should not be empty!'),
+            ]
+        )
+    submit = SubmitField('Upload')
+
+@app.route('/uploads/<filename>', methods=['GET', 'POST'])
+def get_file(filename):
+    return send_from_directory(app.config['UPLOADED_IMAGES_DEST'], filename)
+    
+
+
+@app.route('/upload', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return render_template('index.html', error='No file part')
-
-        file = request.files['file']
-
-        if file.filename == '':
-            return render_template('index.html', error='No selected file')
-
-        if file:
-            # Save the uploaded file
-            filename = file.filename
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)
-            # Classify
-            predicted_class = model.classify_image(file_path)
-            return render_template('index.html', text=predicted_class)
-    return render_template('index.html')
+    form = UploadForm()
+    if form.validate_on_submit():
+        filename = images.save(form.image.data)
+        file_url = url_for('get_file', filename=filename)
+        predicted_class = model.classify_image(os.path.join(app.config['UPLOADED_IMAGES_DEST'], filename))
+        return render_template('upload.html', form=form, text=predicted_class, file_url=file_url)
+    return render_template('upload.html', form=form)
   
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
